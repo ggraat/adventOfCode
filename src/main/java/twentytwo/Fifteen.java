1b3c8e48a2d291fc81f8a8b8006e4c33688e377c
Geert Graat <g.graat@blueriq.com>
2022-12-15 21:44:34 +0100

    Day 15: part two, fast enough

--- src/main/java/twentytwo/Fifteen.java
+++ src/main/java/twentytwo/Fifteen.java
@@ -3,9 +3,12 @@ package twentytwo;
 import common.Assignment;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Fifteen implements Assignment<String> {
@@ -64,13 +67,19 @@ public class Fifteen implements Assignment<String> {
   public String partTwo() {
     long total = 0;
     for (int y = 0; y <= max; y++) {
-      Set<Integer> intersect = new HashSet<>();
-      fillSet(intersect);
+      Ranges ranges = new Ranges(new Range(0, max));
       for (Sensor sensor : field.sensors) {
-        intersect.removeAll(sensor.intersectionWithLine(y));
+        Range intersection = sensor.intersectWithLine(y, max);
+        if (intersection != null) {
+          ranges.subtract(intersection);
+          if (ranges.isEmpty()) {
+            break;
+          }
+        }
       }
-      if (intersect.size() == 1) {
-        total = ((long) intersect.iterator().next() * 4_000_000) + y;
+      if (ranges.containsUniqueElement()) {
+        total = (ranges.getResult() * 4_000_000L) + y;
+        break;
       }
       System.out.println(y);
     }
@@ -127,5 +136,61 @@ public class Fifteen implements Assignment<String> {
       return intersect;
     }
 
+    public Range intersectWithLine(int yPosition, int max) {
+      int deltaY = Math.abs(yPosition - y);
+      if (deltaY <= distance) {
+        return new Range(Math.max(0, x - (distance - deltaY)), Math.min(x + (distance - deltaY), max));
+      }
+      return null;
+    }
+  }
+
+  private static class Ranges {
+    List<Range> ranges = new ArrayList<>();
+
+    public Ranges(Range range) {
+      ranges.add(range);
+    }
+
+    public void subtract(Range r) {
+      ranges = ranges.stream().map(range -> range.subtract(r)).flatMap(Collection::stream).collect(Collectors.toList());
+    }
+
+    public boolean isEmpty() {
+      return ranges.isEmpty();
+    }
+
+    public boolean containsUniqueElement() {
+      return ranges.size() == 1 && ranges.get(0).start == ranges.get(0).end;
+    }
+
+    public int getResult() {
+      return ranges.get(0).start;
+    }
+  }
+  private static class Range {
+    int start;
+    int end;
+
+    public Range(int start, int end) {
+      this.start = start;
+      this.end = end;
+    }
+
+    public List<Range> subtract(Range r) {
+      if (r.end < start || r.start > end) {
+        return List.of(this);
+      }
+      if (r.start <= start && r.end >= end) {
+        return Collections.emptyList();
+      }
+      if (r.start <= start) {
+        return List.of(new Range(r.end + 1, end));
+      }
+      if (r.end < end) {
+        return List.of(new Range(start, r.start - 1), new Range(r.end + 1, end));
+      }
+      return List.of(new Range(start, r.start - 1));
+    }
   }
 }
